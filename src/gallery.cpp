2d28bb8242696c74e90bfa194c4ad9fad0e030d8
mundus <mundus@pc6.home>
2023-09-19 16:43:06 +0200

    U Cioci

--- src/gallery.cpp
+++ src/gallery.cpp
@@ -372,6 +372,10 @@ void imageSaveTickImplied(){
       release_fb();
       return;
     }
+    else {
+      //{{{notify user}}}, {{{play alarm}}}
+
+    }
   }
 
   Serial.println("saveImage();");
